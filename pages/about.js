d6b7e416808a177ac1e1615a6f3891af5d7241d7
Lucas Aho <aholucascode@gmail.com>
2021-02-12 10:29:14 -0700

    First commit, nextjs feels good af

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "./components/layout";
+
+const About = () => (
+    <div>
+        <Layout/>
+        <h1>Hello World</h1>
+    </div>
+);
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "./components/layout";
+
+const Index = () => (
+    <Layout>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </Layout>
+);
+export default Index;
